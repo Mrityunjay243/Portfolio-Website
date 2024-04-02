11b34c42eb2bd18286b8a57d3a43892fb0112ac1
Mrityunjay243 <mrityunjayjoshi565@gmail.com>
2024-04-02 12:08:59 -0400

    updated urls again

--- src/constants/endpoints.js
+++ src/constants/endpoints.js
@@ -1,13 +1,13 @@
 const endpoints = {
-  navbar: 'https://github.com/Mrityunjay243/Portfolio-Website/blob/main/public/profile/navbar.json',
-  routes: 'https://github.com/Mrityunjay243/Portfolio-Website/blob/main/public/profile/routes.json',
-  home: 'https://github.com/Mrityunjay243/Portfolio-Website/blob/main/public/profile/home.json',
-  social: 'https://github.com/Mrityunjay243/Portfolio-Website/blob/main/public/profile/social.json',
-  about: 'https://github.com/Mrityunjay243/Portfolio-Website/blob/main/public/profile/about.json',
-  skills: 'https://github.com/Mrityunjay243/Portfolio-Website/blob/main/public/profile/skills.json',
-  education: 'https://github.com/Mrityunjay243/Portfolio-Website/blob/main/public/profile/education.json',
-  experiences: 'https://github.com/Mrityunjay243/Portfolio-Website/blob/main/public/profile/experiences.json',
-  projects: 'https://github.com/Mrityunjay243/Portfolio-Website/blob/main/public/profile/projects.json',
+  navbar: 'public/profile/navbar.json',
+  routes: 'public/profile/routes.json',
+  home: 'public/profile/home.json',
+  social: 'public/profile/social.json',
+  about: 'public/profile/about.json',
+  skills: 'public/profile/skills.json',
+  education: 'public/profile/education.json',
+  experiences: 'public/profile/experiences.json',
+  projects: 'public/profile/projects.json',
 };
 
 export default endpoints;
