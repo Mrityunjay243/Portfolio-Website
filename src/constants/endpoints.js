407341f2f9ea92d7d0c0fab54e5fe19c5c5976c8
Mrityunjay243 <mrityunjayjoshi565@gmail.com>
2024-04-03 12:49:27 -0400

    setting up gh-pages

--- src/constants/endpoints.js
+++ src/constants/endpoints.js
@@ -1,13 +1,13 @@
 const endpoints = {
-  navbar: '/Portfolio-Website/profile/navbar.json',
-  routes: '/Portfolio-Website/profile/routes.json',
-  home: '/Portfolio-Website/profile/home.json',
-  social: '/Portfolio-Website/profile/social.json',
-  about: '/Portfolio-Website/profile/about.json',
-  skills: '/Portfolio-Website/profile/skills.json',
-  education: '/Portfolio-Website/profile/education.json',
-  experiences: '/Portfolio-Website/profile/experiences.json',
-  projects: '/Portfolio-Website/profile/projects.json',
+  navbar: '/Portfolio-Website/public/profile/navbar.json',
+  routes: '/Portfolio-Website/public/profile/routes.json',
+  home: '/Portfolio-Website/public/profile/home.json',
+  social: '/Portfolio-Website/public/profile/social.json',
+  about: '/Portfolio-Website/public/profile/about.json',
+  skills: '/Portfolio-Website/public/profile/skills.json',
+  education: '/Portfolio-Website/public/profile/education.json',
+  experiences: '/Portfolio-Website/public/profile/experiences.json',
+  projects: '/Portfolio-Website/public/profile/projects.json',
 };
 
 export default endpoints;
