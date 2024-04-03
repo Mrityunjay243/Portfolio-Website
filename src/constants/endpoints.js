00c849c7e5cb3b001facb31322694ee7b522a847
Mrityunjay243 <mrityunjayjoshi565@gmail.com>
2024-04-02 22:46:02 -0400

    Minor changes to check errors

--- src/constants/endpoints.js
+++ src/constants/endpoints.js
@@ -1,13 +1,13 @@
 const endpoints = {
-  navbar: 'public/profile/navbar.json',
-  routes: 'public/profile/routes.json',
-  home: 'public/profile/home.json',
-  social: 'public/profile/social.json',
-  about: 'public/profile/about.json',
-  skills: 'public/profile/skills.json',
-  education: 'public/profile/education.json',
-  experiences: 'public/profile/experiences.json',
-  projects: 'public/profile/projects.json',
+  navbar: '/Portfolio-Website/profile/navbar.json',
+  routes: '/Portfolio-Website/profile/routes.json',
+  home: '/Portfolio-Website/profile/home.json',
+  social: '/Portfolio-Website/profile/social.json',
+  about: '/Portfolio-Website/profile/about.json',
+  skills: '/Portfolio-Website/profile/skills.json',
+  education: '/Portfolio-Website/profile/education.json',
+  experiences: '/Portfolio-Website/profile/experiences.json',
+  projects: '/Portfolio-Website/profile/projects.json',
 };
 
 export default endpoints;
