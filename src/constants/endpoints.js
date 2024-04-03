4e53a44e8c66f76e57b8f2280f3e56b631a42040
Mrityunjay243 <mrityunjayjoshi565@gmail.com>
2024-04-03 12:59:34 -0400

    shortening the end point path

--- src/constants/endpoints.js
+++ src/constants/endpoints.js
@@ -1,13 +1,13 @@
 const endpoints = {
-  navbar: '/Portfolio-Website/public/profile/navbar.json',
-  routes: '/Portfolio-Website/public/profile/routes.json',
-  home: '/Portfolio-Website/public/profile/home.json',
-  social: '/Portfolio-Website/public/profile/social.json',
-  about: '/Portfolio-Website/public/profile/about.json',
-  skills: '/Portfolio-Website/public/profile/skills.json',
-  education: '/Portfolio-Website/public/profile/education.json',
-  experiences: '/Portfolio-Website/public/profile/experiences.json',
-  projects: '/Portfolio-Website/public/profile/projects.json',
+  navbar: 'profile/navbar.json',
+  routes: 'profile/routes.json',
+  home: 'profile/home.json',
+  social: 'profile/social.json',
+  about: 'profile/about.json',
+  skills: 'profile/skills.json',
+  education: 'profile/education.json',
+  experiences: 'profile/experiences.json',
+  projects: 'profile/projects.json',
 };
 
 export default endpoints;
